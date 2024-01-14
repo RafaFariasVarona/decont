5824f1e653a618c2ba090772da6f1dfd6b000e4a
Rafa Farias <rafafariasupv@gmail.com>
2024-01-14 18:30:15 +0100

    Merge the fastqs from the same sample into a single file

--- scripts/merge_fastqs.sh
+++ scripts/merge_fastqs.sh
@@ -3,3 +3,13 @@
 # stored in the output directory specified by the second argument ($2).
 #
 # The directory containing the samples is indicated by the first argument ($1).
+if [ "$#" -ne 3 ]
+then
+    echo "Usage: $0 <samples_directory> <output_directory> <sample_id>"
+    exit 1
+fi
+samdir=$1
+outdir=$2
+sampleid=$3
+mkdir -p ~/decont/$outdir
+cat ~/decont/$samdir/${sampleid}*.fastq.gz > ~/decont/$outdir/${sampleid}.fastq.gz
